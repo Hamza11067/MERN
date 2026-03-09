ba0f7e00fa81824562cd3b0be40e5b9701085e13
Hamza11067 <hamzakhalid1067@gmail.com>
2026-03-09 11:47:36 +0500

    Home section completed

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return (
+    <div>
+      <div className="flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm">
+        <div>
+          <img src={assets.logo} className="mb-5 w-32 " alt="" />
+          <p className="w-full md:w-2/3 text-gray-600">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat,
+            illum?
+          </p>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5 ">COMPANY</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>Home</li>
+            <li>About Us</li>
+            <li>Delivery</li>
+            <li>Privacy Policy</li>
+          </ul>
+        </div>
+
+        <div>
+          <p className="text-xl font-medium mb-5">GET IN TOUCH</p>
+          <ul className="flex flex-col gap-1 text-gray-600">
+            <li>+92323 1045234</li>
+            <li>hamzakhalid1067@gmail.com</li>
+          </ul>
+        </div>
+      </div>
+      <div>
+        <hr />
+        <p className="py-5 text-sm text-center">
+          Copyright {year} @forever.com - All Rights Reserved
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;
